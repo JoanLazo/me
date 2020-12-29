1b68a62dbdf3a4e49ab4ffeb51070139e74c276b
JoanLazo <joan162581@gmail.com>
2020-12-28 20:26:28 -0500

    cambios carrucel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -91,6 +91,28 @@ export class HomeComponent implements OnInit {
         $(".game_over").trigger('play');
       }
   }, 40000);
+
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:true
+          },
+          600:{
+              items:3,
+              nav:false
+          },
+          1000:{
+              items:5,
+              nav:true,
+              loop:false
+          }
+      }
+  })
+
   }
   
 
